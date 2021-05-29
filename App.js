821a0501d3592a4741b95b6932a2c78e57027d67
vinit <dhullvinit15@gmail.com>
2021-05-29 12:30:12 +0530

    node module commit

--- App.js
+++ App.js
@@ -3,8 +3,9 @@ import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
-  return (
     console.log("here23wew");
+  return (
+  
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
